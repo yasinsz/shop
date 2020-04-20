e645067918140d1e53f7e70ef73cc45340cf8ace
yasinsz <yasin.sz@hotmail.com>
2020-04-20 10:00:57 +0200

    add show products

--- app/Http/Controllers/Frontend/CategoryController.php
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Product;
 use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
     public function show(Category $category)
     {
-        $products = \App\Product::all();
+        $products = Product::paginate(8);
 
         return view('frontend/categories/show', [
             'category' => $category,
