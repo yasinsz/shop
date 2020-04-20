d5e010370c004b5a53e2a86adb007b377ee84aa0
yasinsz <yasin.sz@hotmail.com>
2020-04-20 14:29:52 +0200

    add manytomany

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     public $fillable = ['name', 'price', 'description', 'msrp', 'stock'];
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
 }
